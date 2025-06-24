e3c69a4727363800e633c555cad54d3aaef9d929
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-06-23 21:54:06 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7viifv2hVZenTc7FX2f_fhusrMb_QbEg",
-  authDomain: "morleys-restaurant.firebaseapp.com",
-  projectId: "morleys-restaurant",
-  storageBucket: "morleys-restaurant.firebasestorage.app",
-  messagingSenderId: "430306650328",
-  appId: "1:430306650328:web:ecfe9f04bf7b9d17154fad",
-  measurementId: "G-JF16BTQ2Q9"
-};
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
 
-// Export auth so we can use it in other scripts
-window.auth = auth;
-window.firebaseFns = {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-};
+  // Make them global
+  window.auth = auth;
+  window.db = db;

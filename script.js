4f7938441db75fcff3ac37bcf8308f69a6a2b242
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-06-23 21:23:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -513,3 +513,18 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7viifv2hVZenTc7FX2f_fhusrMb_QbEg",
+  authDomain: "morleys-restaurant.firebaseapp.com",
+  projectId: "morleys-restaurant",
+  storageBucket: "morleys-restaurant.firebasestorage.app",
+  messagingSenderId: "430306650328",
+  appId: "1:430306650328:web:ecfe9f04bf7b9d17154fad",
+  measurementId: "G-JF16BTQ2Q9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

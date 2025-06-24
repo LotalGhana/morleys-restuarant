f06368a76b08bc14d0ead285a18588125175630a
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-06-24 02:50:51 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,14 @@
-const firebaseConfig = {
+ // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+  // Make them global
+  window.auth = auth;
+  window.db = db;
+
+<script>
+  const firebaseConfig = {
   apiKey: "AIzaSyD7viifv2hVZenTc7FX2f_fhusrMb_QbEg",
   authDomain: "morleys-restaurant.firebaseapp.com",
   projectId: "morleys-restaurant",
@@ -7,13 +17,19 @@ const firebaseConfig = {
   appId: "1:430306650328:web:ecfe9f04bf7b9d17154fad",
   measurementId: "G-JF16BTQ2Q9"
 };
-
+  };
 
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
   const db = firebase.firestore();
 
-  // Make them global
+  // Expose globally for script.js
   window.auth = auth;
   window.db = db;
+  window.firebaseFns = {
+    createUserWithEmailAndPassword: firebase.auth().createUserWithEmailAndPassword,
+    signInWithEmailAndPassword: firebase.auth().signInWithEmailAndPassword,
+    signOut: firebase.auth().signOut
+  };
+</script>
